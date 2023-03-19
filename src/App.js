c7ec1d53d9eb3ede6cc00663b50367c154fea44e
Roman M <rmarchen95@gmail.com>
2023-03-19 02:27:00 -0700

    SUSPENSE
    implemented suspense loading option on travel page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
+
 
 function App() {
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
-      <div className="contents">
-          <AboutUs />
-          <Packages />
-      </div>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <AboutUs />
+        <Packages />
+      </Suspense>
     </div>
   );
 }
